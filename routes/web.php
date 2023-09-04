ef207ae64f21f7d7422ed285f8919b3f10042e58
Noseilos <jonaseguiron@gmail.com>
2023-09-05 00:51:38 +0800

    added AdminRole middleware

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,8 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin');
+// Admin Group Middleware
+Route::middleware(['auth', 'roles:admin'])->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin');
+});
+// End Admin Group Middleware
